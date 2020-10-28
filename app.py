1f05b38ab44acf636d3e5030e942b5cc180b66ab
Dev Verma <devvermatheone@gmail.com>
2020-10-28 22:37:37 +0530

    Changes made

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
-@app.route("/bot", methods=["POST"])
+@app.route("/bot", methods=["POST", 'GET'])
 def response():
-    query = dict(request.form)['query']
-    res = query + " " + time.ctime()
-    return jsonify({"response" : res})
+    if request.method == 'POST':
+        query = dict(request.form)['query']
+        res = query + " " + time.ctime()
+        return jsonify({"response" : res})
+    else:
+        return "<h1>You are not suppossed to be here<h1>"
     
 if __name__=="__main__":
     app.run(threaded=True, port=5000)
