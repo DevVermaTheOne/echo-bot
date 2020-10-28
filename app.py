2ff883c7301e3005526a41666f1f1ae0a044d619
Dev Verma <devvermatheone@gmail.com>
2020-10-28 22:22:52 +0530

    added welcome message

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify,request
 import time
 
-app = Flask(__name__);
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 
 @app.route("/bot", methods=["POST"])
 def response():
@@ -10,4 +14,4 @@ def response():
     return jsonify({"response" : res})
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0",)
+    app.run(threaded=True, port=5000)
